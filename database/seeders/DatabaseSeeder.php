6c812e6c7903e441155e3e5473be1f275d1c3370
chandrawira <chandrawira@gmail.com>
2022-11-17 07:51:29 +0700

    api-key db init

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ApiKey;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        ApiKey::factory(1)->create(); //create API sample data
     }
 }
