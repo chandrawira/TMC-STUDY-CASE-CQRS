c34a8d32042c303a7444beb0d744a03b3843e8d1
chandrawira <chandrawira@gmail.com>
2022-11-18 23:32:11 +0700

    lelaaah

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -2,17 +2,37 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProductTest extends TestCase
 {
+    //use RefreshDatabase;
+
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_ProductCreation()
     {
-        $this->assertTrue(true);
+        
+        $response =  $this->withHeaders([
+            'Authorization' => 'GeSAXqfDomWFnDrSVedo0JgrKPPX9IRa0tbkvEuNtBkTO76IHLph7f9AGUUpcC9a',
+        ])
+        ->json('POST', '/api/product', [
+            'sku'  => $sku = 'FO-491-53-17352',
+            'name'  =>  $name = time().'test',
+            'price'  =>  $price = '1000',
+            'stock'  =>  $stock = '12',
+            'categoryId'  =>  $categoryId = Str::uuid()->toString(),
+        ]);
+
+        Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(200);
+
     }
 }
